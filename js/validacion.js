7f883aac4e6653f27ae62ec25878d43214d8d2b9
agalcor <agalcor1911@g.educaand.es>
2025-06-05 12:35:19 +0200

    a

--- js/validacion.js
+++ js/validacion.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
+document.addEventListener('DOMContentLoaded', function() {    
     document.getElementById('singup').addEventListener('click',validateSignupForm);
     document.getElementById('login-btn').addEventListener('click', validateLoginForm);
-}); 
+});
+
 function validateSignupForm() {
     event.preventDefault();
     const name = document.getElementById('name').value.trim();
@@ -11,15 +11,16 @@ function validateSignupForm() {
     const confipass = document.getElementById('confirm_password').value.trim();
     const box = document.getElementById('box1').checked;
 
-    const regexemail = /^[A-Za-z]{6,8}\d{3,4}@g\.educaand\.es$/;
+    const regexemail = /^[a-z]{7}\d{3,4}@g\.educaand\.es$/;
     const regexpass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/;
 
-    if (!box) return Swal.fire("Error", "No has aceptado la política de privacidad", "error");
-    if (!name || !email || !password || !confipass || !box) return Swal.fire("Error", "No puede haber campos vacíos", "error");
+   
+    if (!name || !email || !password || !confipass) return Swal.fire("Error", "No puede haber campos vacíos", "error");
     if (name.length < 3) return Swal.fire("Error", "El nombre debe tener al menos 3 caracteres", "error");
     if (!regexemail.test(email)) return Swal.fire("Error", "Formato de email requerido: usuario123@g.educaand.es", "error");
     if (!regexpass.test(password)) return Swal.fire("Error", "La contraseña debe contener 8-16 caracteres, al menos 1 mayúscula, 1 minúscula y 1 número y 1 caracter especial(!@#$%^&*)", "error");
     if (password !== confipass) return Swal.fire("Error", "Las contraseñas no coinciden", "error");
+    if (!box) return Swal.fire("Error", "No has aceptado la política de privacidad", "error");
 
     Swal.fire("¡Éxito!", "Registro completado", "success");
 }
